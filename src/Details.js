bb606be2c0bfba8a2c889dc1c068ae30bebaca75
ben west <benwest0291@outlook.com>
2021-10-28 15:51:51 +0100

    added react router and created a details page

---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2> Tiggs and Simba</h2>;
+};
+
+export default Details;
--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
